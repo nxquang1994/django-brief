87473436a406b10a4cc788105f8c904dab949dd1
nxquang1994 <nguyenxuanquang1994@gmail.com>
2020-08-30 22:35:56 +0700

    Add basic configuration and CRUD

--- brief_site/brief_site/settings.py
+++ brief_site/brief_site/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order.apps.OrderConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'brief_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'passonate',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '0.0.0.0',
+        'PORT': '3307'
     }
 }
 
---
+++ brief_site/order/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Order(models.Model):
+    category = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    status = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.name
+    pass
+
+class Delivery(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    delivery_name = models.CharField(max_length=255)
+    start_delivery_date = models.DateTimeField('Start delivery date')
+    finish_delivery_date = models.DateTimeField('Finish delivery date')
+    def __str__(self):
+        return self.delivery_name
+    pass
+
+class Goods(models.Model):
+    goods_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.goods_name
+    pass
+
+class DeliveryGoods(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, on_delete=models.CASCADE)
+    note = models.TextField()
+    pass
